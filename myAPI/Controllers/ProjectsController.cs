e095f265b2c58b43a1ce2ef0f813890f41a874a9
goktemkirez <ogkirez@gmail.com>
2022-08-04 14:22:56 +0300

    Project update, delete, getbyid actions

--- myAPI/Controllers/ProjectsController.cs
+++ myAPI/Controllers/ProjectsController.cs
@@ -25,5 +25,49 @@ namespace myAPI.Controllers
             manager.TAdd(project);
             return Ok(project);
         }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var project = manager.TGetById(id);
+            if(project == null)
+                return NotFound();
+            else
+                return Ok(project);
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var project = manager.TGetById(id);
+            if (project == null)
+                return NotFound();
+            else
+                manager.TDelete(project);
+                return Ok();
+        }
+
+
+        [HttpPut]
+        public IActionResult Update(Project project)
+        {
+            if(project == null)
+                return BadRequest(project);
+            var selectedProject = manager.TGetById(project.ProjectID);
+            if (selectedProject == null)
+                return NotFound();
+            else
+            {
+                selectedProject.ProjectID = project.ProjectID;
+                selectedProject.Title = project.Title;
+                selectedProject.Detail = project.Detail;
+                selectedProject.VideoUrl = project.VideoUrl;
+                selectedProject.CodeUrl = project.CodeUrl;
+                selectedProject.LiveUrl = project.LiveUrl;
+                manager.TUpdate(selectedProject);
+                return Ok();
+            }
+        }
     }
 }
